3971d743f48155eb214ac7d5fa14f302877996d6
João Paulo Rocha <joao.rochap03@gmail.com>
2022-09-14 22:43:00 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let light = document.querySelector('.light')
+let body = document.querySelector('body')
+let projects = document.querySelector('.buttons')
+light.onclick = function(){
+    body.classList.toggle('active')
+}
+projects.onclick = function(){
+    document.querySelector('#projects').classList.toggle('more')
+}
+
+const inicial = document.querySelector('h3')
+function typeWriter(element){
+    const textArray = element.innerHTML.split('')
+    element.innerHTML = ''
+    setTimeout(function(){
+        textArray.forEach((letra, i) => { //Para cada elemento um index
+            setTimeout(function(){
+                element.innerHTML += letra
+            }, 75 * i)
+        })
+    }, 2000)
+}   
+typeWriter(inicial)
+
+window.scrl = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '20px' });
+//ScrollReveal de #home
+scrl.reveal('.contentMain', {duration: 3000})
+//ScrollReveal de #about
+scrl.reveal('.programacao', {duration : 4000, origin: 'right'})
+scrl.reveal('.contentAbout', {duration : 2000, origin: 'left'})
+//ScrollReveal de #projects
+scrl.reveal('.contentProjects', {duration: 2000, origin: 'top'})
